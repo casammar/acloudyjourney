19e9d67f65c06f489eba8775f34811bcf64028b5
Chris Sammarco <chris.sammarco@gmail.com>
2020-08-04 22:55:09 -0400

    change background color of app

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,12 +53,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `A Cloudy Journey`,
+        short_name: `Chris's Dev Journal`,
+        description: `Notes about the cloud`,
         start_url: `/`,
         background_color: `#fff`,
-        theme_color: `#6B46C1`,
+        theme_color: `#20ad00`,
         display: `standalone`,
         icons: [
           {
